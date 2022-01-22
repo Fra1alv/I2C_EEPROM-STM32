908635bb738c59513edfd5229424d7be3d1ba672
A.Freire <78938104+Fra1alv@users.noreply.github.com>
2022-01-22 15:50:38 -0500

    Update I2C_EEPROM.h

--- I2C_EEPROM.h
+++ I2C_EEPROM.h
@@ -22,85 +22,89 @@
 #ifndef INC_I2C_EEPROM_H_
 #define INC_I2C_EEPROM_H_
 
-/*BEGIN**************************INCLUDES************************************/
+/*BEGIN--------------------------includes-------------------------------------*/
 #include "stdint.h"
 #include "stddef.h"
 #include "stm32f3xx_hal.h"
 #include "main.h"
 #include "math.h"
-/*END****************************INCLUDES************************************/
+/*END----------------------------includes-------------------------------------*/
 /*
  *
  */
-/*BEGIN**************************MEMORY I2C *********************************/
+/*BEGIN--------------------------typedef--------------------------------------*/
 extern I2C_HandleTypeDef hi2c1;
 #define I2C_EEPROM &hi2c1
 
-/*END************************************************************************/
+/*END----------------------------typedef--------------------------------------*/
 /*
  *
  */
-/*BEGIN**************************COMM STATUS*********************************/
+/*BEGIN--------------------------define---------------------------------------*/
+/*COSTANTS*/
+
+/*Communication Status*/
+
 #define COMM_OK				0
 #define COMM_ERROR			1
 #define EEPROM_MEM_SIZE_ERR	2
-/*END************************************************************************/
-/*
- *
- */
-/*BEGIN**************************MEMORY IC ADDRESS***************************/
-/*COSTANTS*/
+/*---------------------------*/
+
+/*Memory configuration*/
+
+/*I2C_EEPROM address*/
+
 #define A0PIN_BIT			1
 #define A1PIN_BIT			2
 #define A2PIN_BIT			3
 
-/*END************************************************************************/
-/*
- *
- */
-/*BEGIN**************************MEMORY CONFIGURATION************************/
-
-/*Address Pins configuration*/
 /*Guide:
- * Set  MEMORY_A0_PIN value to 0 or to 1 according hardware configuration*/
+ * Set  EEPROM_A0_PIN value to 0 or to 1 according hardware configuration*/
 
-#define EEPROM_A0_PIN 		0								// Address Pin A0
-#define EEPROM_A1_PIN 		0								// Address Pin A1
-#define EEPROM_A2_PIN 		0								// Address Pin A2
-#define EEPROM_WP_PORT		GPIOB				// Peripheral Port
-#define	EEPROM_WP_PIN		6									// Write Protection Pin
+#define EEPROM_A0_PIN 		0		// Address Pin A0
+#define EEPROM_A1_PIN 		0		// Address Pin A1
+#define EEPROM_A2_PIN 		0		// Address Pin A2
+#define EEPROM_WP_PORT		GPIOB	// Peripheral Port
+#define	EEPROM_WP_PIN		6		// Write Protection Pin
+
+#define DEV_ADDRESS			(0XA0 | (EEPROM_A2_PIN << A2PIN_BIT) | (EEPROM_A1_PIN << A1PIN_BIT) | (EEPROM_A0_PIN << A0PIN_BIT))		// IC memory address
+/*-----------------------------------------------------------------------------------------------------------------------------*/
 
 /*Memory configuration*/
-#define DEV_ADDRESS			(0XA0 | (EEPROM_A2_PIN << A2PIN_BIT) | (EEPROM_A1_PIN << A1PIN_BIT) | (EEPROM_A0_PIN << A0PIN_BIT))		// IC memory address
+
 #define EEPROM_ADD_SIZE		2U		//EEPROM address size
 #define	EEPROM_PAGE_SIZE	128U	//EEPROM page size
 #define EEPROM_NUM_OF_PAGE	512U	//EEPROM number of pages
-#define EEPROM_pPAGE_ADD	log(EEPROM_PAGE_SIZE)/log(2)
 #define EEPROM_STWC			5U		//EEPROM Self time write cycle
-#define	EEPROM_MAX_SIZE		((EEPROM_PAGE_SIZE)*(EEPROM_NUM_OF_PAGE)) //EEPROM maximum memory size
 #define EEPROM_SIZE_ERROR	2		// EEPROM error value
 #define TRAILS				5		//Number of test used for connection test
 #define TIMEOUT				1000	// Time out
 
-/*END************************************************************************/
-/*
- *
- */
-/*BEGIN**************************IMPORT HAL DRIVER FUNCTIONS***********/
+#define EEPROM_pPAGE_ADD	log(EEPROM_PAGE_SIZE)/log(2) // Pointer for page address
+
+#define	EEPROM_MAX_SIZE		((EEPROM_PAGE_SIZE)*(EEPROM_NUM_OF_PAGE)) //EEPROM maximum memory size
+/*----------------------------------------------------------------------------*/
+
+/*Functions*/
 
 #define _GPIO_PIN_RESET										GPIO_PIN_RESET
-#define _GPIO_PIN_SET            GPIO_PIN_SET
-#define _DELAY_STWC(void)								HAL_Delay(EEPROM_STWC)
+#define _GPIO_PIN_SET										GPIO_PIN_SET
+#define _DELAY_STWC(void)									HAL_Delay(EEPROM_STWC)
 
-#define I2C_EEPROM_READY(void)            					       HAL_I2C_IsDeviceReady(I2C_EEPROM,DEV_ADDRESS,TRAILS,TIMEOUT)
-#define	I2C_EEPROM_WRITE(da, ma, mas, md, qu, tim)			 HAL_I2C_Mem_Write(I2C_EEPROM, (da), (ma), (mas), (md), (qu), (tim))
-#define I2C_EEPROM_READ(da, ma, mas, md, qu, tim)			  HAL_I2C_Mem_Read(I2C_EEPROM, (da), (ma), (mas), (md), (qu), (tim))
-#define I2C_EEPROM_WP(sp)									                    HAL_GPIO_WritePin(EEPROM_WP_PORT,(1 << EEPROM_WP_PIN), (sp))
+#define I2C_EEPROM_READY(void)            					HAL_I2C_IsDeviceReady(I2C_EEPROM,DEV_ADDRESS,TRAILS,TIMEOUT)
+#define	I2C_EEPROM_WRITE(da, ma, mas, md, qu, tim)			HAL_I2C_Mem_Write(I2C_EEPROM, (da), (ma), (mas), (md), (qu), (tim))
+#define I2C_EEPROM_READ(da, ma, mas, md, qu, tim)			HAL_I2C_Mem_Read(I2C_EEPROM, (da), (ma), (mas), (md), (qu), (tim))
+#define I2C_EEPROM_WP(sp)									HAL_GPIO_WritePin(EEPROM_WP_PORT,(1 << EEPROM_WP_PIN), (sp))
 
-/*BEGIN**************************** Prototype *********************************/
+/*------------------------------------------------------------------------------------------------------------------------------*/
+
+/*END----------------------------define---------------------------------------*/
 /*
  *
  */
+
+/*BEGIN-----------------------functions prototypes----------------------------*/
+
 /*BEGIN**************************MEMORY READY *********************************/
 /*
  * Description: Function for test if memory is ready for communication
@@ -178,6 +182,6 @@ uint8_t I2C_EEPROM_Erase(uint32_t address, uint16_t size);
 /*
  *
  */
-/*END****************************** Prototype *********************************/
+/*END-------------------------functions prototypes----------------------------*/
 
 #endif /* INC_I2C_EEPROM_H_ */
